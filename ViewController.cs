2456d82ced9c2abc9b8deb6f3ae924411d11f040
miocr <owner@Yosemite.local>
2017-01-31 10:09:52 +0100

    Init
    Init

---
+++ ViewController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace CocoaXamarinMacApp1
+{
+	public partial class ViewController : NSViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			// Do any additional setup after loading the view.
+		}
+
+		public override NSObject RepresentedObject {
+			get {
+				return base.RepresentedObject;
+			}
+			set {
+				base.RepresentedObject = value;
+				// Update the view, if already loaded.
+			}
+   
+		
+		}
+	
+		partial void btnPridatClicked(NSButton sender)
+		{
+			levelIndicator.IntValue++;
+		}
+
+		partial void btnUbratClicked(NSButton sender)
+		{
+			levelIndicator.IntValue--;
+		}
+	
+	}
+
+
+}
---
+++ ViewController.designer.cs
@@ -0,0 +1,32 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio Community to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace CocoaXamarinMacApp1
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		AppKit.NSLevelIndicator levelIndicator { get; set; }
+
+		[Action ("btnPridatClicked:")]
+		partial void btnPridatClicked (AppKit.NSButton sender);
+
+		[Action ("btnUbratClicked:")]
+		partial void btnUbratClicked (AppKit.NSButton sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (levelIndicator != null) {
+				levelIndicator.Dispose ();
+				levelIndicator = null;
+			}
+		}
+	}
+}
